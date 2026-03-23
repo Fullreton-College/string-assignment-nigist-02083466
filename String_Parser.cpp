eeb7a0874ca0c7fcc24958ddbc1c88be9db7ccf2
Tekle, Nigist <nigist.tekle21@gmail.com>
2026-03-23 14:54:08 -0700

    code changes complete

--- String_Parser.cpp
+++ String_Parser.cpp
@@ -1,13 +1,46 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <sstream> // For parsing lines
+#include <sstream>
 using namespace std;
 
-void parseCSV(const string&);
+void parseCSV(const string& filename) {
+    ifstream file(filename);
+
+    if (!file) {
+        cout << "Error opening file\n";
+        return;
+    }
+
+    string line;
+
+    while (getline(file, line)) {
+        string name, age, initial, isStudent, grade;
+        stringstream ss(line);
+        string token;
+
+        while (ss >> token) {
+            if (token.find("Name:") == 0)
+                name = token.substr(5);
+            else if (token.find("Age:") == 0)
+                age = token.substr(4);
+            else if (token.find("Initial:") == 0)
+                initial = token.substr(8);
+            else if (token.find("IsStudent:") == 0)
+                isStudent = token.substr(10);
+            else if (token.find("Grade:") == 0)
+                grade = token.substr(6);
+        }
+
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+        cout << "Initial: " << initial << endl;
+        cout << "IsStudent: " << isStudent << endl;
+        cout << "Grade: " << grade << endl << endl;
+    }
+}
 
 int main() {
-   parseCSV("students.csv");
-
-   return 0;
+    parseCSV("students.csv");
+    return 0;
 }
